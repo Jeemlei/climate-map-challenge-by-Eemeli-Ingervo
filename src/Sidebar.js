f6c770f84a61162419de2314250d5e33bf8bc2d8
Jeemlei <eemeli.ingervo@gmail.com>
2023-03-31 17:33:20 +0300

    Removed broken locationGetter
    locationGetter prevented Sidebar from rendering the information of the marker that was clicked most recently.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
-import getSelectedLocatoinId from './locationGetter';
 
 function Sidebar({selectedLocationId, observationLocations}) {
-    const id = getSelectedLocatoinId(selectedLocationId);
 
-    const loc = observationLocations.find(loc => loc.info.id === id);
+    const loc = observationLocations.find(loc => loc.info.id === selectedLocationId);
     return <div>
         <pre>{loc && JSON.stringify(loc.info, null, 4)}</pre>
     </div>
