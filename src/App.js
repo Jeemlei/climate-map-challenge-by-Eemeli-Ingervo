8aeae5b13b8e6f3f65c4ff15c5a13103f9d7bcdc
Jeemlei <eemeli.ingervo@gmail.com>
2023-03-31 17:21:01 +0300

    Removed unnecessary tile format variable from basemap URL

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
   const map = (
     <MapContainer center={position} zoom={6}>
       <TileLayer
-        url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+        url='https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
         subdomains='abcd'
         maxZoom={19}
