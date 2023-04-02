b1e86c58c5004bfd4bb2d5341055740115dee4d6
Jeemlei <eemeli.ingervo@gmail.com>
2023-04-02 14:19:53 +0300

    Highlighting selected marker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Map, Marker, TileLayer } from 'react-leaflet';
-import styled from 'styled-components';
-import L from 'leaflet';
-import Sidebar from './Sidebar';
 import { fetchObservationLocations } from '../utils';
-
-const MapContainer = styled(Map)`
-	width: calc(100vw - 500px);
-	height: 100vh;
-	position: absolute;
-	top: 0px;
-	left: 500px;
-`;
-
-// Ugly hack to fix Leaflet icons with leaflet loaders
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-	iconUrl: require('leaflet/dist/images/marker-icon.png'),
-	shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
+import Sidebar from './Sidebar';
+import Map from './Map';
 
 function App() {
 	const [observationLocations, setObservationLocations] = useState([]);
@@ -27,33 +9,19 @@ function App() {
 
 	useEffect(() => fetchObservationLocations(setObservationLocations), []);
 
-	const position = [65, 26];
-	const map = (
-		<MapContainer center={position} zoom={6}>
-			<TileLayer
-				url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
-				attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> 
-                     &copy; <a href="https://carto.com/attributions">CARTO</a>'
-				subdomains="abcd"
-				maxZoom={19}
-			/>
-			{observationLocations.map(loc => (
-				<Marker
-					position={[loc.position.lat, loc.position.lon]}
-					key={loc.info.id}
-					onClick={() => setSelectedLocation(loc.info.id)}
-				/>
-			))}
-		</MapContainer>
-	);
-
 	return (
 		<div>
 			<Sidebar
 				selectedLocationId={selectedLocation}
 				observationLocations={observationLocations}
 			/>
-			{map}
+			<Map
+				center={[65, 26]}
+				zoom={6}
+				locations={observationLocations}
+				selectedLocation={selectedLocation}
+				setSelectedLocation={setSelectedLocation}
+			/>
 		</div>
 	);
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Map as LeafletMap, Marker, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+import styled from 'styled-components';
+
+const MapContainer = styled(LeafletMap)`
+	width: calc(100vw - 500px);
+	height: 100vh;
+	position: absolute;
+	top: 0px;
+	left: 500px;
+`;
+
+const defaultIcon = L.icon({
+	iconRetinaUrl: require('../assets/images/marker-icon-2x.png'),
+	iconUrl: require('../assets/images/marker-icon.png'),
+	shadowUrl: require('../assets/images/marker-shadow.png'),
+	iconSize: [25, 41],
+	iconAnchor: [12.5, 41],
+});
+
+const selectedIcon = L.icon({
+	iconRetinaUrl: require('../assets/images/marker-icon-selected-2x.png'),
+	iconUrl: require('../assets/images/marker-icon-selected.png'),
+	shadowUrl: require('../assets/images/marker-shadow.png'),
+});
+
+const Map = ({
+	center,
+    zoom,
+	locations,
+	selectedLocation,
+	setSelectedLocation,
+}) => {
+	return (
+		<MapContainer center={center} zoom={zoom}>
+			<TileLayer
+				url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+				attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> 
+                     &copy; <a href="https://carto.com/attributions">CARTO</a>'
+				subdomains="abcd"
+				maxZoom={20}
+                minZoom={3}
+			/>
+			{locations.map(loc => (
+				<Marker
+					position={[loc.position.lat, loc.position.lon]}
+					key={loc.info.id}
+					icon={selectedLocation === loc.info.id ? selectedIcon : defaultIcon}
+					onClick={() => setSelectedLocation(loc.info.id)}
+				/>
+			))}
+		</MapContainer>
+	);
+};
+
+export default Map;
